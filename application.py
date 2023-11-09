d7841d51edbf66583e958d2483924eefd91369e9
하 <123haeun@naver.com>
2023-11-09 08:38:58 +0000

    commit

--- application.py
+++ application.py
@@ -44,8 +44,13 @@ def reg_item_submit():
 
 @application.route("/submit_item_post", methods=['POST'])
 def reg_item_submit_post():
+    
+    image_file=request.files["file"]
+    image_file.save("static/images/{}".format(image_file.filename))
     data = request.form
-    return render_template("submit_item_result.html", data=data)
+    
+    return render_template("submit_item_result.html", data=data, img_path="static/images/{}".format(image_file.filename))
+
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+    application.run(host='0.0.0.0', debug=True)
